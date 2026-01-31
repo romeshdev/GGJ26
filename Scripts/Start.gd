81c09b17e9b629207cc7f5068c5abe646f122d87
romesh <romesh.panditha@gmail.com>
2026-01-31 13:08:29 +1100

    tape pulling

--- Scripts/Start.gd
+++ Scripts/Start.gd
@@ -5,3 +5,7 @@ func _on_play_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_rope_test_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/RopeTest.tscn")
