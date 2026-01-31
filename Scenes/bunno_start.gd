3b067095dfffb97c7d6e408c38cf8d70c76b2338
Stephanie Everett <anuxinamoon@gmail.com>
2026-01-31 19:02:53 +1100

    Added wall texture

--- Scenes/bunno_start.gd
+++ Scenes/bunno_start.gd
@@ -3,4 +3,4 @@ signal doorHit
 
 func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
 	doorHit.emit()
-	$CollisionShape2D.set_deferred("disabled", true)
+	$CollisionShape2D.call_deferred("disabled", true)
