d566412f2ef9a0e9c8a16c815e08e88e2e2b951c
Stephanie Everett <anuxinamoon@gmail.com>
2026-02-02 10:47:29 +1100

    Animated Tape wall, redirected crab

--- Scripts/Start.gd
+++ Scripts/Start.gd
@@ -3,9 +3,6 @@ extends Control
 
 
 
-func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
-
 func _on_quit_button_pressed():
 	get_tree().quit()
 
@@ -15,4 +12,4 @@ func _on_rope_test_button_pressed():
 
 
 func _on_bunnings_door_hit() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/pick_tape_wall.tscn")
---
+++ Scripts/tape.gd
@@ -0,0 +1,5 @@
+extends RigidBody2D
+
+
+func _ready():
+	$AnimatedSprite2D.play()
